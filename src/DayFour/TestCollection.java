05eb6e04d62bf6e4540fa549bc94047cf8edba50
88-Rising <348514635@qq.com>
2019-11-30 22:54:59 +0800

    读容器泛型_底层源码

---
+++ src/DayFour/TestCollection.java
@@ -0,0 +1,24 @@
+package DayFour;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+* 在容器的存储数据，读取数据是都避免了大量的类型判断 非常便捷
+*
+*
+* 泛型类在集合中的使用
+* */
+public class TestCollection {
+    //list,set,map,Itertor都是与容器相关的接口
+
+
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<String>();
+
+        Map map=new HashMap();
+    }
+
+}
