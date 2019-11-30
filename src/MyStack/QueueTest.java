3f93bac96188c92b710b72055924224ac4b9425c
88-Rising <348514635@qq.com>
2019-12-01 00:58:44 +0800

    队列的基本概念

--- src/MyStack/QueueTest.java
+++ src/MyStack/QueueTest.java
@@ -1,4 +1,8 @@
 package MyStack;
+
+import java.util.Deque;
+import java.util.Queue;
+
 /*
 *队列(Queue)简称队，和堆栈一样，也是一种运算受限的线性表
 * 其限制是仅允许在表的一端进行插入，而在表的另一端进行删除
@@ -12,6 +16,17 @@ package MyStack;
 * 队列的常用的操作是 enqueue：数据元素入队
 *                   dequeue:队首元素出队
 *                    peek：取队首元素
+* 队列的存储结构有： 顺序队列：使用数组作为存储结构
+*                    循环队列：避免出队之后数组前几个存储空间为空    双端队列：两端都可以进队或者出队
+*双端队列既可以用队列来操作也可以用实现栈来操作
+*
+*
 * */
 public class QueueTest {
+    public static void main(String[] args) {
+        Deque dq;//可以在两端插入或删除元素的队列
+        Queue qu;//Queue接口的实现类有LinkedList,ArrayDeque
+    }
+
+
 }
