93fe888fe640f160f1a485c41a37878f3f5cc54a
88-Rising <348514635@qq.com>
2020-02-16 09:46:25 +0800

    并发编程 模拟12306买票系统(线程不安全测试)

---
+++ src/ThreadTest/UnSafe.java
@@ -0,0 +1,38 @@
+package ThreadTest;
+
+public class UnSafe {
+    public static void main(String[] args) {
+        UnsafeWeb12306 web=new UnsafeWeb12306();
+
+        new Thread(web,"码农").start();
+        new Thread(web,"码畜").start();
+        new Thread(web,"码蝗").start();
+
+
+
+
+
+    }
+    public static class UnsafeWeb12306 implements Runnable{
+        private int ticketNums=99;
+        private boolean flag=true;
+        @Override
+        public void run() {
+            while(flag){
+                test();
+            }
+        }
+        public void test(){
+            if(ticketNums<0){
+                flag=false;
+                return;
+            }
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName()+"-->"+ticketNums--);
+        }
+    }
+}
